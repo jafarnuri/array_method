0a96cdfea4166219428399da82ba09c5d5a28e4c
jafarnuri <jafarnuri1994@gmail.com>
2024-10-11 21:11:11 +0400

    adding array_methods

---
+++ index.php
@@ -0,0 +1,162 @@
+//Array methods
+<?php
+
+// $array1 = ['Cafar', 'Kenan', 'Nofel', 'Ilkin'];
+// $array2 = ['Aygun', 'Hecer', 'Zinyet', 'Nofel', 'Tamilla'];
+// $array_map = ['1', '6', '9', '2'];
+//Array_push()
+// array_push($array1, "Nofel");
+
+// echo '<pre>';
+// print_r($array1);
+// echo '</pre>';
+
+
+//Array_pop()
+
+// array_pop($array1);
+// echo '<pre>';
+// print_r($array1);
+// echo '</pre>';
+
+//Array_shift()
+
+// array_shift($array2);
+// echo '<pre>';
+// print_r($array2);
+// echo '</pre>';
+
+//Array_unshift()
+
+// array_unshift($array2, "Aygun");
+// echo '<pre>';
+// print_r($array2);
+// echo '</pre>';
+
+//Array_slice()
+
+// $array_slice = array_slice($array2, 1, 3);
+// echo '<pre>';
+// print_r($array_slice);
+// echo '</pre>';
+
+//Array_splice()
+
+// $array_splice = array_splice($array2, 1, 1, "Hecer");
+// echo '<pre>';
+// print_r($array_splice);
+// echo '</pre>';
+
+
+//Array_merge()
+
+// $array_merge = array_merge($array1, $array2);
+// echo '<pre>';
+// print_r($array_merge);
+// echo '</pre>';
+
+//Array_diff()
+
+// $array_diff = array_diff($array1, $array2);
+// echo '<pre>';
+// print_r($array_diff);
+// echo '</pre>';
+
+//Array_keys()
+
+// $array_keys = array_keys($array1);
+// echo '<pre>';
+// print_r($array_keys);
+// echo '</pre>';
+
+//Array_values()
+
+// $array_values = array_values($array1);
+// echo '<pre>';
+// print_r($array_values);
+// echo '</pre>';
+
+//in_Array()
+
+// $in_array = in_array("Nofel", $array1);
+// echo '<pre>';
+// print_r($in_array);
+// echo '</pre>';
+
+//Array_map()
+
+// $array_maps = array_map(function ($n) {
+//     return $n * $n;
+// }, $array_map);
+
+// echo '<pre>';
+// print_r($array_maps);
+// echo '</pre>';
+
+//Array_filter()
+
+// $array_filter = array_filter(
+
+//     $array_map,
+//     function ($n) {
+//         return $n % 2 == 0;
+//     }
+// );
+// echo '<pre>';
+// print_r($array_filter);
+// echo '</pre>';
+
+//Array_reduce()
+
+// $array_reduce = array_reduce(
+//     $array_map,
+//     function ($x, $y) {
+//         return $x + $y;
+//     }
+// );
+// echo '<pre>';
+// print_r($array_reduce);
+// echo '</pre>';
+
+//Count()
+
+// $count = count($array2);
+// echo '<pre>';
+// print_r($count);
+// echo '</pre>';
+
+//Sort()
+
+// sort($array_map);
+// echo '<pre>';
+// print_r($array_map);
+// echo '</pre>';
+
+//Rsort()
+
+// rsort($array_map);
+// echo '<pre>';
+// print_r($array_map);
+// echo '</pre>';
+
+//Asort()
+
+// asort($array_map);
+// echo '<pre>';
+// print_r($array_map);
+// echo '</pre>';
+
+//Ksort()
+
+// ksort($array_map);
+// echo '<pre>';
+// print_r($array_map);
+// echo '</pre>';
+
+//Array_flip()
+
+// $array_flip = array_flip($array1);
+// echo '<pre>';
+// print_r($array_flip);
+// echo '</pre>';
+?>
